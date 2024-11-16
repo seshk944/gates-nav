482b1764d7eb127c67e7a50f4df93085e66f5510
AlexisLuo25 <alexisluo@hotmail.com>
2024-11-15 22:38:26 -0500

    Co-authored-by: joshuaI8 <joshuaI8@users.noreply.github.com> Co-authored-by: seshk944 <seshk944@users.noreply.github.com> Co-authored-by: yana-157 <yana-157@users.noreply.github.com>

--- main.py
+++ main.py
@@ -11,98 +11,22 @@ import heapq
 # https://realpython.com/python-heapq-module/
 
 
-# fifth floor map
-map = """
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-X..........................................
-X.........X....X....X....X....X....X....XXX
-X....XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX....XXX
-X....X..............................X....X
-X....X....X....X....X....X....X....X....X
-X....X....X....X....X....X....X....X....X
-X....X..............................X....X
-X....X....X....X....X....X....X....X....X
-X....XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX....XXX
-X....X..............................X....X
-X....X....X....X....X....X....X....X....X
-X....X....X....X....X....X....X....X....X
-X...XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-"""
-
-def parse_map(map):
-    lines = map.splitlines()
-    origin = 0, 0
-    destination = len(lines[-1]) - 1, len(lines) - 1
-    return lines, origin, destination
-
-def is_valid(lines, position):
-    x, y = position
-    if not (0 <= y < len(lines) and 0 <= x < len(lines[y])):
-        return False
-    if lines[y][x] == "X":
-        return False
-    return True
-
-def get_neighbors(lines, current):
-    x, y = current
-    for dx in [-1, 0, 1]:
-        for dy in [-1, 0, 1]:
-            if dx == 0 and dy == 0:
-                continue
-            position = x + dx, y + dy
-            if is_valid(lines, position):
-                yield position
-
-def get_shorter_paths(tentative, positions, through):
-    path = tentative[through] + [through]
-    for position in positions:
-        if position in tentative and len(tentative[position]) <= len(path):
-            continue
-        yield position, path
-
-
-# those are all helper functions above it
-def find_path(map):
-    lines, origin, destination = parse_map(map)
-    tentative = {origin: []}
-    candidates = [(0, origin)]
-    certain = set()
-    while destination not in certain and len(candidates) > 0:
-        _ignored, current = heapq.heappop(candidates)
-        if current in certain:
-            continue
-        certain.add(current)
-        neighbors = set(get_neighbors(lines, current)) - certain
-        shorter = get_shorter_paths(tentative, neighbors, current)
-        for neighbor, path in shorter:
-            tentative[neighbor] = path
-            heapq.heappush(candidates, (len(path), neighbor))
-    if destination in tentative:
-        return tentative[destination] + [destination]
-    else:
-        raise ValueError("no path")
-
-def show_path(path, map):
-    lines = map.splitlines()
-    for x, y in path:
-        lines[y] = lines[y][:x] + "@" + lines[y][x + 1 :]
-    return "\n".join(lines) + "\n"
-
-
-path = find_path(map)
-print(show_path(path, map))
-
-
 ####################################################################################################################
 
 # process video, frame by frame --> compare each frame to user's image
-# cam = cv2.VideoCapture('video url')
-# if not cap.isOpened():
-#     print('Error')
-#  while cap.isOpened():
-#     ret, frame = cap.read
-#     if ret:
-#         cv2.imshow('Frame', frame)
+cap = cv2.VideoCapture('video url')
+
+if not cap.isOpened():
+    print('Error')
+ while cap.isOpened():
+    ret, frame = cap.read
+    if ret:
+        cv2.imshow('Frame', frame)
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
 # video_frames = {some function that returns all the frames in a list or something}
 
